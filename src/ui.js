143b7353220b4680b6478a0408f6f2fe0b34933a
Nick Stark <dev.nickstark@gmail.com>
2015-02-09 11:32:30 -0600

    added docs for delegateEvent

--- src/ui.js
+++ src/ui.js
@@ -6,10 +6,12 @@ define(function(require) {
 
     return {
         // delegate event to common root. NOTE: blur/focus and other events may not bubble
+        //   var delegate = delegateEvent(document.body, 'click', 'a.print', printPage);
+        //   delegate.remove(); // deregister function is returned by method
         delegateEvent: require('./ui/delegateEvent'),
 
         // fallback for vendor prefixed or non-existant Element.prototype.matches
-        // var isElementActive = matches(element, '.clasName_isActive');
+        //   var isElementActive = matches(element, '.clasName_isActive');
         matches: require('./ui/matches'),
 
         // calls offsetWidth to trigger reflow
@@ -20,7 +22,7 @@ define(function(require) {
         triggerReflow: require('./ui/triggerReflow'),
 
         // returns a unique id with optional prefix (always a string)
-        // newInput.id = uniqueId('input'); // 'input7'
+        //   newInput.id = uniqueId('input'); // 'input7'
         uniqueId: require('./ui/uniqueId')
     };
 
--- src/ui/delegateEvent.js
+++ src/ui/delegateEvent.js
@@ -2,28 +2,30 @@ define(function(require) {
     'use strict';
 
     var matches = require('./matches');
+    var removeListener = Element.prototype.removeEventListener;
 
     return function(root, eventName, childSelector, handler, capturePhase) {
-        capturePhase = capturePhase || false;
+        capturePhase = capturePhase || false; // default to bubbling phase
 
         var delegateHandler = function(event) {
             var target = event.target;
+
+            // check if any element up the chain matches selector
             while (target !== root) {
                 if (target && matches(target, childSelector)) {
-                    handler.call(this, event);
+                    handler.call(this, event); // call handler with original event
                     break;
                 }
                 target = target.parentNode;
             }
         };
 
+        // add delegate-wrapped handler
         root.addEventListener(eventName, delegateHandler, capturePhase);
 
         // return function to remove event listener
         return {
-            remove: function() {
-                return Element.prototype.removeEventListner.bind(root, eventName, delegateHandler, capturePhase);
-            }
+            remove: removeListener.bind(root, eventName, delegateHandler, capturePhase)
         };
     };
 
