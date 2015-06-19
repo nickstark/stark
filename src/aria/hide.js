d96cb197b46d2ddf44ea09ff205dd3c99b3e14cc
Nick Stark <dev.nickstark@gmail.com>
2015-06-19 14:52:42 -0500

    removed unused aria branch

--- src/aria/hide.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-    'use strict';
-
-    return function(element) {
-        element.setAttribute('hidden', 'hidden');
-        element.setAttribute('aria-hidden', 'true');
-    };
-
-});
--- src/aria/show.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-    'use strict';
-
-    return function(element) {
-        element.removeAttribute('hidden');
-        element.setAttribute('aria-hidden', 'false');
-    };
-
-});
